5d97b7c5aee5f1388f52c424b3d2aed1067268de
Jideotetic <jideotetic@gmail.com>
2025-09-18 00:05:51 +0100

    Some cleaning up

--- src/components/dashboard/DashboardLayoutContainer.jsx
+++ src/components/dashboard/DashboardLayoutContainer.jsx
@@ -1,7 +1,3 @@
 export default function DashboardLayoutContainer({ children }) {
-  return (
-    <div className=" min-h-screen border-2 border-red-500 bg-[#F7F9FD]">
-      {children}
-    </div>
-  );
+  return <div className="min-h-screen bg-[#F7F9FD]">{children}</div>;
 }
