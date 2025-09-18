9b5bbfb22d649f7a0b1c5ea0275e4e172874fd5c
Jideotetic <jideotetic@gmail.com>
2025-09-18 07:48:58 +0100

    Some cleaning up

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,6 +8,7 @@ import About from "@/components/About";
 import Testimonial from "@/components/Testimonial";
 import ExploreTask from "@/components/ExploreTask";
 import FeaturedCommunities from "@/components/FeaturedCommunities";
+import Metrics from "@/components/Metrics";
 
 function HomePage() {
   const location = useLocation();
@@ -29,37 +30,7 @@ function HomePage() {
     <>
       <Hero />
 
-      <div className="mx-auto w-full max-w-[1200px] space-y-8 bg-white px-5 py-10 md:space-y-16 md:pt-[104px]">
-        <div className="mx-auto max-w-[1200px] space-y-4 text-center text-[#050215]">
-          <h2 className="text-[32px] font-extrabold md:text-[44px]">
-            A Growing Network of Builders
-          </h2>
-
-          <p className="mx-auto max-w-[800px] text-[18px] font-light md:text-[20px]">
-            Join communities, complete tasks, and earn rewards alongside
-            thousands of contributors.
-          </p>
-        </div>
-
-        <div className="grid grid-cols-2 gap-1 md:grid-cols-4 lg:px-10 [@media(max-width:379px)]:grid-cols-1">
-          {METRICS.map((metric, i) => (
-            <div
-              key={i}
-              className={`flex flex-col justify-center rounded-[8px] bg-[#F7F9FD] px-[20px] py-[40px] [@media(max-width:379px)]:items-center ${metric.title === "paid out" && "sm:order-4 [@media(max-width:379px)]:order-4"}`}
-            >
-              <div
-                className={`${metric.title === "communities" || metric.title === "contributors" ? "text-[#2F0FD1]" : "text-[#1082E4]"} text-[32px] font-bold lg:text-[48px]`}
-              >
-                {metric.title === "paid out" && "$"}
-                {metric.value} {metric.title !== "contributors" && "+"}
-              </div>
-              <div className="text-[15px] text-[#636366] lg:text-[20px]">
-                {metric.title}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+      <Metrics />
 
       <About />
 
